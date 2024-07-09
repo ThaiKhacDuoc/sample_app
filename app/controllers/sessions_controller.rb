f953de4a80155e0184f566674e4e6c3e922f7ac9
ThaiKhacDuoc <khacduockhamduc@gmail.com>
2024-07-09 09:39:15 +0700

    [Naitei] Chapter_9
    [Naitei] Chapter_9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    if user&.authenticate(params.dig(:session, :password))
-      log_in user
-      redirect_to user
+    if @user&.authenticate(params.dig(:session, :password))
+      log_in @user
+      params.dig(:session, :remember_me) == "1" ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = t "sessions.create.invalid"
       render :new, status: :unprocessable_entity
