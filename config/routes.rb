a9feddde2289805a90b5f27d49d2775e0ea126d6
ThaiKhacDuoc <khacduockhamduc@gmail.com>
2024-07-15 23:30:59 +0700

    [Naitei] Chapter_11

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
     get "/help", to: "static_pages#help"
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
-    resources :users, only: :show
+    resources :users
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    resources :account_activations, only: :edit
   end
 end
